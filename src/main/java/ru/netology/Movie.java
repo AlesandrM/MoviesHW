b19b0f1779c1bdf23fd57a995cbe10af80d3f668
Aleksandr Madzharov <madzharov-aleks@mail.ru>
2022-03-12 19:00:50 +0500

    first commit

---
+++ src/main/java/ru/netology/Movie.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+public class Movie {
+    private int id;
+    private String imageUrl;
+    private String name;
+    private String genre;
+
+    public Movie(int id, String imageUrl, String name, String genre) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.genre = genre;
+    }
+}
